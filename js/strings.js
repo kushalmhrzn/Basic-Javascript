6bbdb2594a3d4ad40444393be932d24a051506a2
kushalnam <30662908+kushalnam@users.noreply.github.com>
2019-07-02 08:16:51 +0545

    strings

---
+++ js/strings.js
@@ -0,0 +1,14 @@
+// the string in js are used for the storing and manipulating text
+
+var x = "Kushal Maharjan"; //double quotation
+var y = 'twenty-one years old' // single quotation 
+
+// the value can be declare in single or double quotation.
+
+console.log(x+y);
+
+//to write the "" inside "" the below is the example
+
+var myName = 'My name is "Kushal Maharjan"';
+
+console.log(myName);
